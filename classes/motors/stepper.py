e98c89dcae3b3a715d923d433e5a32eed0caefce
Marten Mehide <marten@space.local>
2023-05-17 17:39:45 +0300

    Added startup sequence

--- classes/motors/stepper.py
+++ classes/motors/stepper.py
@@ -1,13 +1,19 @@
 """Stepper motor class file."""
+from gpiozero import OutputDevice
+from time import sleep
 
 
 class Stepper(object):
-    def __init__(self, step: float):
+    def __init__(self, step_pin: int, direction_pin: int, step: float):
         """
         Initialize stepper motor.
-
+        
+        :param step_pin: pin that sends out step control signals
+        :param direction_pin: pin that controls direction of stepper motor
         :param step: how many degrees stepper motor turns with a single step
         """
+        self.step_sender = OutputDevice(step_pin, active_high=True)
+        self.direction = OutputDevice(direction_pin)
         self.step = step
 
     def rotate_steps(self, steps: int):
@@ -18,4 +24,12 @@ class Stepper(object):
 
         :param steps: how many steps to take (positive or negative value)
         """
-        pass
+        if steps >= 0:
+            self.direction.off()
+        else:
+            self.direction.on()
+
+        for i in range(abs(steps)):
+            self.step_sender.off()
+            sleep(0.03)
+            self.step_sender.on()
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from constants import FIRST_SERVO_PIN, SECOND_SERVO_PIN, STEPPER_STEP_DEGREES, FIRST_ARM_LENGTH_CM, SECOND_ARM_LENGTH_CM, \
-                      ARM_HEIGHT_CM
+from gpiozero import Button
+
+from constants import *
 
 from classes.motors.servo import Servo
 from classes.motors.stepper import Stepper
@@ -9,7 +10,12 @@ from classes.robotarm import RobotArm
 if __name__ == "__main__":
     first_servo = Servo(FIRST_SERVO_PIN)
     second_servo = Servo(SECOND_SERVO_PIN)
-    stepper = Stepper(STEPPER_STEP_DEGREES)
+    
+    stepper = Stepper(STEPPER_STEP_PIN, STEPPER_DIRECTION_PIN, STEPPER_STEP_DEGREES)
+    # Zero the stepper motor
+    detector = Button(STEPPER_ZERO_POSITION_DETECT_PIN)
+    while not detector.is_pressed():
+        stepper.rotate_steps(1)
 
     arm = RobotArm(FIRST_ARM_LENGTH_CM, SECOND_ARM_LENGTH_CM, ARM_HEIGHT_CM, stepper, first_servo, second_servo)
     
@@ -17,4 +23,4 @@ if __name__ == "__main__":
     while True:
         arm.move_arm_to_position(12, 20, 10)
         time.sleep(2)
-        arm.move_arm_to_position(12, 40, 5)
+        arm.move_arm_to_position(20, 40, 5)
