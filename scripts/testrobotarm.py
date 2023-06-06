d6e12204e2f14e42b79900e07f61255aaf98ba4d
Marten Mehide <marten@space.local>
2023-06-06 06:58:58 +0300

    a

--- scripts/testrobotarm.py
+++ scripts/testrobotarm.py
@@ -22,12 +22,12 @@ if __name__ == "__main__":
     
     stepper = Stepper(STEPPER_STEP_PIN, STEPPER_DIRECTION_PIN, STEPPER_STEP_DEGREES)
     # Zero the stepper motor
-    detector = Button(STEPPER_ZERO_POSITION_DETECT_PIN)
-    while not detector.is_pressed:
-        stepper.rotate_steps(1)
+    # detector = Button(STEPPER_ZERO_POSITION_DETECT_PIN)
+    # while not detector.is_pressed:
+    #     stepper.rotate_steps(1)
 
     magnet = ElectroMagnet(ELECTROMAGNET_PULL_PIN, ELECTROMAGNET_PUSH_PIN, MAGNET_PULL_DISTANCE_CM, ELECTROMAGNET_HEIGHT_CM)
 
     arm = RobotArm(FIRST_ARM_LENGTH_CM, SECOND_ARM_LENGTH_CM, ARM_HEIGHT_CM, stepper, first_servo, FIRST_ARM_ZERO_POSITION_OFFSET_ANGLE_DEGREES, second_servo, SECOND_ARM_ZERO_POSITION_OFFSET_ANGLE_DEGREES, magnet)
 
-    arm.move_arm_to_position(-130, 25, 20)
+    arm.move_arm_to_position(0, 25, 20)
