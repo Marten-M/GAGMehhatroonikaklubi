523cd9401e21eff7c7ab3d32f3bd31502e595c92
Marten Mehide <marten@space.local>
2023-06-06 06:29:26 +0300

    a

--- src/classes/chessrobot/robotarm/robotarm.py
+++ src/classes/chessrobot/robotarm/robotarm.py
@@ -51,8 +51,8 @@ class RobotArm(object):
         third_side = ((self.height - height) ** 2 + distance_from_arm ** 2) ** 0.5
         area = get_area_heron(self.first_arm_length, self.second_arm_length, third_side)
 
-        a = 90 + -self.first_servo_zero_position_offset_angle + 180 - (arcsin(distance_from_arm / third_side) + get_angle_between_triangle_sides(area, self.first_arm_length, third_side))
-        b = -self.second_servo_zero_position_offset_angle + 180 - get_angle_between_triangle_sides(area, self.first_arm_length, self.second_arm_length)
+        a = 270 - self.first_servo_zero_position_offset_angle - (arcsin(distance_from_arm / third_side) + get_angle_between_triangle_sides(area, self.first_arm_length, third_side))
+        b = 180 - get_angle_between_triangle_sides(area, self.first_arm_length, self.second_arm_length)
 
         return (int(a), int(b))
 
