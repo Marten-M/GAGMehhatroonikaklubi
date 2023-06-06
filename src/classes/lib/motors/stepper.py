4029de4b34ef78b822492e2189316d34fad4e993
Marten Mehide <marten@space.local>
2023-06-06 05:56:52 +0300

    a

--- src/classes/lib/motors/stepper.py
+++ src/classes/lib/motors/stepper.py
@@ -31,5 +31,5 @@ class Stepper(object):
 
         for i in range(abs(steps)):
             self.step_sender.off()
-            sleep(0.0005)
+            sleep(0.001)
             self.step_sender.on()
